d291fe9c3a4f6bcc45d7b0a0dc48cdfa15ccded9
Loreli <dejesus.loreli@gmail.com>
2023-05-19 15:01:33 +1000

    useFavIcon hook from react-use

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
-import logo from "./wiki-logo.png"
+import { useFavicon } from 'react-use';
+import logo from "./wiki-logo.png";
+
 
 export default function App() {
 
@@ -10,8 +12,9 @@ export default function App() {
 
   const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=extracts&exchars=250&exintro=true&explaintext=true&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchItem}`;
 
+  useFavicon(logo);
 
-const handSubmit = (e) => {
+  const handSubmit = (e) => {
   console.log('submit called');
     
     e.preventDefault();
@@ -58,7 +61,11 @@ const handSubmit = (e) => {
         <div className="relative mb-4 flex w-1/3 flex-wrap items-stretch justify-center mx-auto my-auto">
           <input
             type="search"
-            className="relative m-0 block w-[1px] min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+            className="relative m-0 block w-[1px] min-w-0 flex-auto rounded border border-solid border-button-hover bg-transparent 
+            bg-clip-padding px-3 py-[0.5rem] text-base font-normal leading-[1.8] text-button-hover 
+            outline-none transition duration-200 ease-in-out focus:z-[3]
+             focus:border-button-beige focus:text-button-hover focus:shadow-[inset_0_0_0_2px_rgb(59,113,202)] 
+             focus:outline-none dark:border-button-hover dark:text-button-hover placeholder:text-button-beige dark:focus:border-button-hover"
             placeholder="What are you searching for?"
             aria-label="Search"
             aria-describedby="button-addon2"
@@ -67,7 +74,7 @@ const handSubmit = (e) => {
             
           />
           <span
-            className="input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-neutral-700 dark:text-neutral-200"
+            className="input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-button-hover dark:text-button-hover"
             onClick={handSubmit}
             id="basic-addon2"
           >
@@ -92,7 +99,7 @@ const handSubmit = (e) => {
           data.map((d, i) => {
             const url = `https://en.wikipedia.org/?curid=${d.pageid}`;
             return (
-              <div className="data-result hover:bg-button-hover hover:relative hover:top-[-2px] hover:left-[-2px] p-4 bg-button-beige border-2 border-solid border-button-brown rounded-lg shadow-button-brown text-button-brown" key={i}>
+              <div className="data-result shadow-md hover:bg-button-hover hover:relative hover:top-[-2px] hover:left-[-2px] p-4 bg-button-beige border-2 border-solid border-button-brown rounded-lg shadow-button-brown text-button-brown" key={i}>
                 <h2 className="font-black">{d.title}</h2>
                 <p dangerouslySetInnerHTML={{ __html: d.snippet }}></p>
                 <a href={url} className="text-xs italic">..Read More</a>
